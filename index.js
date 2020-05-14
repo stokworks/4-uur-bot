bc655f5045996d6a7614a2bc568d518ba6e8794f
Sander Bakkum <sander@sanderbakkum.nl>
2020-05-14 19:37:59 +0200

    Version 1.0.0

---
+++ index.js
@@ -0,0 +1,70 @@
+require('dotenv').config();
+const http = require('http');
+const path = require('path');
+const JSONdb = require('simple-json-db');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const db = new JSONdb(path.join(__dirname, 'guilds.json'), {asyncWrite: true});
+
+const playAudioFile = (channel, file) => {
+	channel.join().then(voiceConnection => {
+		const dispatcher = voiceConnection.play(file);
+		dispatcher.on('finish', end => {
+			channel.leave();
+		});
+	}).catch(err => console.log(err));
+};
+
+const broadcastAudioFile = (file) => {
+	if (bot.readyAt) {
+		bot.guilds.cache.forEach((guild) => {
+			const channelID = db.get(guild.id);
+
+			if (channelID) {
+				const channel = guild.channels.resolve(channelID);
+				
+				if (channel && channel.type == 'voice') {
+					console.info(`Playing audio in guild '${guild.name}' in channel '${channel.name}'`);
+					playAudioFile(channel, file);
+				}
+			}
+		});
+	}
+};
+
+const triggerServer = http.createServer((req, res) => {
+	broadcastAudioFile(path.join(__dirname, '4uur.mp3'));
+	res.end('OK\n');
+});
+
+bot.on('ready', () => {
+	console.info(`Logged in as ${bot.user.tag}`);
+	triggerServer.listen(process.env.TRIGGER_PORT, process.env.TRIGGER_HOST, err => {
+		if (err) return console.log('Could not start trigger server', err);
+		console.info(`Trigger server listening on ${process.env.TRIGGER_HOST}:${process.env.TRIGGER_PORT}`);
+	});
+});
+
+bot.on('message', message => {
+	if (!message.guild || !message.member) return;
+
+	if (message.mentions.has(bot.user)) {
+		// @bot enable
+		if (message.content.toLowerCase().includes('enable')) {
+			if (message.member.voice && message.member.voice.channel) {
+				const channel = message.member.voice.channel;
+				message.reply(`set jingle channel to ${channel.name}.`);
+				db.set(message.guild.id, channel.id);
+			} else {
+				message.reply('please join a voice channel first.');
+			}
+		// @bot disable
+		} else if (message.content.toLowerCase().includes('disable')) {
+			message.reply('disabled jingle.');
+			db.delete(message.guild.id);
+		}
+	}
+});
+
+bot.login(process.env.DISCORD_TOKEN);
